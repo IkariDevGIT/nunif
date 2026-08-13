8e78ea25fb5e31bb44c96b0fec480599396e1cea
nagadomi <nagadomi@gmail.com>
2026-08-13 12:57:42 +0900

    waifu2x: Add support for swin_unet_v3 in export_onnx

--- waifu2x/export_onnx.py
+++ waifu2x/export_onnx.py
@@ -71,7 +71,7 @@ def convert_swin_unet_art(model_dir, output_dir):
                 path.join(out_dir, "scale2x.onnx"))
 
     scale1x = ONNXScale1x(offset=8)
-    scale1x.export_onnx(path.join(out_dir, "scale1x.onnx"))
+    scale1x.export_onnx(path.join(out_dir, "scale1x.onnx"), dynamo=True)
 
 
 def convert_swin_unet_downscaled4x(domain, model_dir, output_dir):
@@ -100,7 +100,7 @@ def convert_swin_unet_downscaled4x(domain, model_dir, output_dir):
     model_2x.export_onnx(path.join(out_dir, "scale2x.onnx"))
 
     scale1x = ONNXScale1x(offset=8)
-    scale1x.export_onnx(path.join(out_dir, "scale1x.onnx"))
+    scale1x.export_onnx(path.join(out_dir, "scale1x.onnx"), dynamo=True)
 
 
 def convert_swin_unet_photo(model_dir, output_dir):
@@ -109,6 +109,27 @@ def convert_swin_unet_photo(model_dir, output_dir):
 
 def convert_swin_unet_art_scan(model_dir, output_dir):
     convert_swin_unet_downscaled4x("art_scan", model_dir, output_dir)
+
+
+def convert_swin_unet_v3_art(model_dir, output_dir):
+    domain = "art"
+    in_dir = path.join(model_dir, "swin_unet_v3", domain)
+    out_dir = path.join(output_dir, "swin_unet_v3", domain)
+    os.makedirs(out_dir, exist_ok=True)
+    for noise_level in (0, 1, 2, 3):
+        export_onnx(path.join(in_dir, f"noise{noise_level}.pth"),
+                    path.join(out_dir, f"noise{noise_level}.onnx"),
+                    dynamo=True)
+        export_onnx(path.join(in_dir, f"noise{noise_level}_scale2x.pth"),
+                    path.join(out_dir, f"noise{noise_level}_scale2x.onnx"),
+                    dynamo=True)
+
+    export_onnx(path.join(in_dir, "scale2x.pth"),
+                path.join(out_dir, "scale2x.onnx"),
+                dynamo=True)
+
+    scale1x = ONNXScale1x(offset=8)
+    scale1x.export_onnx(path.join(out_dir, "scale1x.onnx"), dynamo=True)
 
 
 def convert_utils(output_dir):
@@ -150,6 +171,10 @@ if __name__ == "__main__":
     convert_swin_unet_art(args.input_dir, args.output_dir)
     convert_swin_unet_photo(args.input_dir, args.output_dir)
     convert_swin_unet_art_scan(args.input_dir, args.output_dir)
+
+    if path.isdir(path.join(args.input_dir, "swin_unet_v3", "art")):
+        logger.info("swin_unet_v3")
+        convert_swin_unet_v3_art(args.input_dir, args.output_dir)
 
     logger.info("utils")
     convert_utils(args.output_dir)
